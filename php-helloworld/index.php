8decd6da966ab7932759185dc01bbb35af7b6d9d
Francisco Ruben Jimenez Corzo <rubenj@li-a4bfc44c-3416-11b2-a85c-9394567deaf8.ibm.com>
2022-02-20 11:58:23 -0600

    Cambio al index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,6 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Esta es una nueva linea...\n";
+print "Esta es una nueva linea...\n";
+print "Esta es una nueva linea...\n";
 ?>
