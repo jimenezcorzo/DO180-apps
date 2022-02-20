8c194686a08334aa0b6e6f7eb4954343df565e4f
Francisco Ruben Jimenez Corzo <rubenj@li-a4bfc44c-3416-11b2-a85c-9394567deaf8.ibm.com>
2022-02-20 12:35:17 -0600

    Paco modificando el index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,10 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Esta es una nueva linea...\n";
-print "Esta es una nueva linea...\n";
-print "Esta es una nueva linea...\n";
-print "Probando el webhook desde github sin ssl...\n";
-print "Probando el webhook desde github sin ssl...\n";
-print "Probando el webhook desde github sin ssl...\n";
-print "Probando el webhook desde github sin ssl...\n";
+print "Esta es una nueva linea, para probar el trigger desde el webhook de git, con ssl y con el secreto en la URL principal, app tipo json...\n";
 ?>
