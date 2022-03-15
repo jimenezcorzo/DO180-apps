cd33f718b0cece71ccb6252599e90ed690dcf48f
Francisco Ruben Jimenez Corzo <rubenj@li-a4bfc44c-3416-11b2-a85c-9394567deaf8.ibm.com>
2022-03-15 15:06:04 -0600

    PHP Modificado

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -3,4 +3,6 @@ print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "Una nueva Linea. Hello, World! php version is " . PHP_VERSION . "\n";
 print "Una nueva Linea. Hello, World! php version is " . PHP_VERSION . "\n";
 print "Veamos que rollo...\n";
+print "Veamos que rollo...\n";
+print "Veamos que rollo...\n";
 ?>
