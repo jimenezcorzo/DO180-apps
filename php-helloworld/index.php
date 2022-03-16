5cf2edeb7e4d4ca6219149eee13d4df0898e0393
Francisco Ruben Jimenez Corzo <rubenj@li-a4bfc44c-3416-11b2-a85c-9394567deaf8.ibm.com>
2022-03-16 13:09:24 -0600

    pequeno cambio

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -4,5 +4,5 @@ print "Una nueva Linea. Hello, World! php version is " . PHP_VERSION . "\n";
 print "Una nueva Linea. Hello, World! php version is " . PHP_VERSION . "\n";
 print "Veamos que rollo...\n";
 print "Veamos que rollo...\n";
-print "Veamos que rollo...\n";
+print "Aqui se edito una nueva linea...\n";
 ?>
